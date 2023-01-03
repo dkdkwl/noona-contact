ab30f714e0a59ad93163e246cc4b15ed40c9843a
dkdkwl <dkdkwl@naver.com>
2023-01-03 14:00:33 +0900

    필드 전화번호 이름 스테이트에 저장하기

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form,Button } from 'react-bootstrap'
 
 const ContactForm = () => {
+    const [name,setName] = useState('');
+    const [phoneNumber,setPhoneNumber] = useState('');
+
   return (
     <div>
         <Form>
         <Form.Group className="mb-3" controlId="formName">
             <Form.Label>이름</Form.Label>
-            <Form.Control type="text" placeholder="이름을 입력해주세요." />
+            <Form.Control onChange={(event)=>{setName(event.target.value); console.log(event.target.value)}} type="text" placeholder="이름을 입력해주세요."/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formContact">
             <Form.Label>전화번호</Form.Label>
-            <Form.Control type="number" placeholder="전화번호를 입력해주세요." />
+            <Form.Control onChange={(event)=>{setPhoneNumber(event.target.value); console.log(event.target.value)}} type="number" placeholder="전화번호를 입력해주세요." />
         </Form.Group>
         <Button variant="primary" type="submit">추가</Button>
         </Form>
